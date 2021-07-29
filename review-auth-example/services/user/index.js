30d7bb2995916ed2a4fc610b9b9d775bb7354d0b
Molpe <fran.mscr@gmail.com>
2021-07-29 11:10:26 +0200

    feat: added proper path of update controller

--- review-auth-example/services/user/index.js
+++ review-auth-example/services/user/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const authorizer = require('../../middlewares/authorizer')
+const multer = require('../../configs/multer')
 
 module.exports = db => {
   const profile = require('./profile')
+  const update = require('./update')
 
   router.get('/', authorizer, profile(db))
+  router.put('/', authorizer, multer.single('profilePic'), update(db))
 
   return router
 }
