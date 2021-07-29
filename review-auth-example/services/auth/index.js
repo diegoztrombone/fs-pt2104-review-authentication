ec55772207aa2e48b332de6a45219f1393c81128
Molpe <fran.mscr@gmail.com>
2021-07-29 11:10:09 +0200

    feat: addedproper paths of signout and confirma controllers

--- review-auth-example/services/auth/index.js
+++ review-auth-example/services/auth/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
+const authorizer = require('../../middlewares/authorizer')
 
 module.exports = db => {
   const signin = require('./signin')
   const signup = require('./signup')
+  const signout = require('./signout')
+  const confirm = require('./confirm')
 
   router.post('/signin', signin(db))
   router.post('/signup', signup(db))
+  router.post('/signout', authorizer, signout())
+  router.get('/confirmation/:confirmationToken', confirm(db))
 
   return router
 }
