62c80532f70aceafc95c786c241032bb7670d665
Molpe <fran.mscr@gmail.com>
2021-07-29 11:01:59 +0200

    feat: added query call to retrieve proper user info and return it to client

--- review-auth-example/services/user/profile.js
+++ review-auth-example/services/user/profile.js
@@ -1,11 +1,20 @@
+const { profile } = require('../../queries/user')
+
 module.exports = db => async (req, res, next) => {
   const { email, username } = res.locals.user
+
+  const user = await profile(db, { email, username })
+
+  if (user === false) {
+    return next({ error: new Error('something went wrong') })
+  }
 
   res.status(200).json({
     success: true,
     data: {
-      email,
-      username,
+      email: user.email,
+      username: user.username,
+      pic: user.profile_pic,
     }
   })
 }
